5aa888cf6c3e7fcff00a31b28040ceab82556258
Artem Pozdnyakov <a.pozdnyakov1@g.nsu.ru>
2026-03-16 11:14:43 +0700

    task2_part1

--- task2/parallel_programs/1.cc
+++ task2/parallel_programs/1.cc
@@ -9,67 +9,73 @@ int n = 20000;
 
 int main()
 {
-    std::vector<double> time;
-    for(int h = 0 ; h<4; h++)
+    std::vector<int> threads_num = {2,4,6,8,16,20,40};
+
+    for(const int threads : threads_num)
     {
-        const auto start{std::chrono::steady_clock::now()};
+        std::vector<double> time;
 
-        std::vector<double> a(n*n);
-        std::vector<double> b(n);
-        std::vector<double> c(n);
-
-        for (int j = 0; j < n; j++)
-                b[j] = j;
-
-        #pragma omp parallel
+        for(int h = 0 ; h<100; h++)
         {
+            const auto start{std::chrono::steady_clock::now()};
 
-            int nthreads = omp_get_num_threads();
-            // std::cout<<nthreads<<std::endl;
-            int threadid = omp_get_thread_num();
-            int items_per_thread = n / nthreads;
-            int lb = threadid * items_per_thread;
-            int ub = (threadid == nthreads - 1) ? (n - 1) : (lb + items_per_thread - 1);
+            std::vector<double> a(n*n);
+            std::vector<double> b(n);
+            std::vector<double> c(n);
 
-            for (int i = lb; i <= ub; i++) {
-                for (int j = 0; j < n; j++)
-                    a[i * n + j] = i + j;
-            }
+            // for (int j = 0; j < n; j++)
+            //         b[j] = j;
 
-            // for (int j = lb; j < ub; j++)
-            //     b[j] = j;
-            
-            // #pragma omp barrier
-
-            for (int i = lb; i <= ub; i++)
+            #pragma omp parallel num_threads(threads)
             {
-                double sum = 0.0;
-                for (int j = 0; j < n; j++)
-                    sum += a[i * n + j] * b[j];
-                c[i] = sum;
-            }
-        }
-        const auto end{std::chrono::steady_clock::now()};
-        const std::chrono::duration<double> dur{end-start};
-        std::cout<<"time: "<<dur.count()<<std::endl;
 
-        time.push_back(dur.count());
+                int nthreads = omp_get_num_threads();
+                int threadid = omp_get_thread_num();
+                int items_per_thread = n / nthreads;
+                int lb = threadid * items_per_thread;
+                int ub = (threadid == nthreads - 1) ? (n - 1) : (lb + items_per_thread - 1);
+
+                for (int i = lb; i <= ub; i++) {
+                    for (int j = 0; j < n; j++)
+                        a[i * n + j] = i + j;
+                    b[i] = i;
+                }
+
+                // for (int j = lb; j < ub; j++)
+                //     b[j] = j;
+                
+                #pragma omp barrier
+
+                for (int i = lb; i <= ub; i++)
+                {
+                    double sum = 0.0;
+                    for (int j = 0; j < n; j++)
+                        sum += a[i * n + j] * b[j];
+                    c[i] = sum;
+                }
+            }
+            const auto end{std::chrono::steady_clock::now()};
+            const std::chrono::duration<double> dur{end-start};
+            // std::cout<<"time: "<<dur.count()<<std::endl;
+
+            time.push_back(dur.count());
+
+            double count = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                count+=c[i];
+            }
+
+            std::cout<<count<<std::endl;
+        }
 
         double count = 0;
 
-        for(int i = 0; i < n; i++)
-        {
-            count+=c[i];
-        }
+        for(auto i : time)
+            count+=i;
 
-        std::cout<<count<<std::endl;
+        std::cout<<"num_threads:"<<threads<<" avg_time: "<<count/100;
     }
-
-    double count = 0;
-
-    for(auto i : time)
-        count+=i;
-
-    std::cout<<"avg_time: "<<count/4;
 
 }
