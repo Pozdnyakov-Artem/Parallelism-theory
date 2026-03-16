1f6d56eb5346cd263c105b7b7f335327809cb349
Artem Pozdnyakov <a.pozdnyakov1@g.nsu.ru>
2026-03-16 14:27:13 +0700

    task2

--- task2/linear_programs/linear_prog2.cc
+++ task2/linear_programs/linear_prog2.cc
@@ -25,7 +25,7 @@ int main()
 {
     std::vector<double> time;
 
-    for(int i = 0; i < 4; i++)
+    for(int i = 0; i < 100; i++)
     {
         const auto start{std::chrono::steady_clock::now()};
 
@@ -43,7 +43,7 @@ int main()
     for(auto i : time)
         count+=i;
 
-    std::cout<<"avg_time: "<<count/4;
+    std::cout<<"avg_time: "<<count/100;
 
     return 0;
 }
