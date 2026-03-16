76c1d69cbaa097c00aa1817fb418dbe7f88b78b5
Artem Pozdnyakov <a.pozdnyakov1@g.nsu.ru>
2026-03-16 18:10:10 +0700

    task2

--- task2/linear_programs/linear_prog3.cc
+++ task2/linear_programs/linear_prog3.cc
@@ -4,44 +4,17 @@
 #include <chrono>
 
 double E = 1e-10;
-int N = 20000;
+int N = 10000;
 double lr = 0.9 * (2.0/(N+1));
 
-// double norm(const std::vector<double> &a)
-// {
-//     double ans = 0;
-//     for(int i = 0; i<N; i++)
-//     {
-//         ans+= a[i]*a[i];
-//     }
-
-//     return ans;
-// }
-
-// bool check_func(const std::vector<double> a,const  std::vector<double> &b)
-// {
-//     // double el = norm(a)/norm(b);
-//     double ans1 = 0, ans2 = 0;
-//     for(int i = 0; i<N; i++)
-//     {
-//         ans1+= a[i]*a[i];
-//         ans2+= b[i]*b[i];
-//     }
-
-//     double el = ans1/ans2;
-//     if(el < E) return false;
-
-//     return true;
-// }
-
-void simple_iteration(const std::vector<double> &A, std::vector<double> &x, const std::vector<double> &b)
+void simple_iteration(const std::vector<double> &A, std::vector<double> &x, const std::vector<double> &b, const double norm_b)
 {
-    double norm_b = 0;
+    // double norm_b = 0;
 
-    for(int i = 0; i < N; i++)
-    {
-        norm_b+=b[i]*b[i];
-    }
+    // for(int i = 0; i < N; i++)
+    // {
+    //     norm_b+=b[i]*b[i];
+    // }
 
     std::vector<double> Ax_b(N);
 
@@ -74,12 +47,15 @@ int main()
 {
     std::vector<double> time;
 
-    for(int iter = 0; iter<4; iter++)
+    for(int iter = 0; iter<25; iter++)
     {
 
         std::vector<double> A(N*N);
         std::vector<double> b(N);
         std::vector<double> x(N);
+        double norm_b = 0;
+
+        const auto start{std::chrono::steady_clock::now()};
 
         for(int i = 0; i<N; i++)
         {
@@ -89,12 +65,11 @@ int main()
                 else A[i*N+j] = 1;
             }
             b[i] = N+1;
+            norm_b+=b[i]*b[i];
             x[i] = 0;
         }
 
-        const auto start{std::chrono::steady_clock::now()};
-
-        simple_iteration(A,x,b);
+        simple_iteration(A,x,b, norm_b);
 
         const auto end{std::chrono::steady_clock::now()};
         const std::chrono::duration<double> dur{end-start};
@@ -113,7 +88,7 @@ int main()
     for(int i = 0; i<4; i++)
         count+=time[i];
 
-    std::cout<<"avg_time: "<<count/4<<std::endl;
+    std::cout<<"avg_time: "<<count/25<<std::endl;
 
 
     return 0;
